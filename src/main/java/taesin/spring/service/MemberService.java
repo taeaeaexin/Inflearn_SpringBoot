aa45aeb29ef2360310adfd298a988fb08f02af29
taeaeaexin <kjk06119@gmail.com>
2025-04-21 22:28:44 +0900

    250421 2228

--- src/main/java/taesin/spring/service/MemberService.java
+++ src/main/java/taesin/spring/service/MemberService.java
@@ -4,14 +4,33 @@ import taesin.spring.domain.Member;
 import taesin.spring.repository.MemberRepository;
 import taesin.spring.repository.MemoryMemberRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     // 회원가입
     public Long join(Member member) {
-        //같은 이름 회원가입 방지
-        memberRepository.findByName(member.getName());
+        validateDuplicateMember(member); //중복 회원 검증
+
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
